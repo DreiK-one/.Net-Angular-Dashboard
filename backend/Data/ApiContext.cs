4265e948d0501ad2afef529c780e368fc70f5e74
DreiK-one <Dreikone1@mail.ru>
2022-12-08 11:17:23 +0100

    Add backend project

---
+++ backend/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace backend.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ backend/Data/Entities/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Data.Entities
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+
+    }
+}
