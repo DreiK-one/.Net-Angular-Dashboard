851ebd0ccf93e9fe73743d9adfd59dc500a0b73e
DreiK-one <Dreikone1@mail.ru>
2022-12-08 10:24:53 +0100

    Updated server component

--- frontend/src/app/server/server.component.ts
+++ frontend/src/app/server/server.component.ts
@@ -12,9 +12,29 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color!: string;
+  buttonText!: string;
+
+
   @Input() serverInput!: Server;
 
   ngOnInit(): void {
+    this.setServerAction(this.serverInput.isOnline);
   }
 
+  setServerAction(isOnline: boolean){
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean){
+    this.setServerAction(!onlineStatus);
+  }
 }
