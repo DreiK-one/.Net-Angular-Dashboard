a5760b024aab004874b35b29a9a50f8212158f03
DreiK-one <Dreikone1@mail.ru>
2022-12-08 09:52:22 +0100

    Add orders table

---
+++ frontend/src/app/shared/order.ts
@@ -0,0 +1,9 @@
+import { Customer } from 'src/app/shared/customer';
+
+export interface Order{
+    id: number,
+    customer: Customer,
+    total: number,
+    placed: Date,
+    fulfilled: Date
+}
