8a7ca88de9e85a99e69f7b7bcb5bdde35a70dbdd
DreiK-one <Dreikone1@mail.ru>
2022-12-15 19:08:21 +0100

    Add Customer, Order controller

--- backend/Controllers/OrderController.cs
+++ backend/Controllers/OrderController.cs
@@ -30,13 +30,57 @@ namespace backend.Controllers
             var totalCount = data.Count();
             var totalPages = Math.Ceiling((double)totalCount / pageSize);
 
-            var responcse = new
+            var response = new
             {
                 Page = page,
                 TotalPages = totalPages
             };
 
-            return Ok();
+            return Ok(response);
+        }
+
+        [HttpGet("ByState")]
+        public IActionResult ByState()
+        {
+            var orders = _context.Orders.Include(_o => _o.Customer).ToList();
+
+            var groupedResult = orders.GroupBy(o => o.Customer.State)
+                .ToList()
+                .Select(grp => new
+            {
+                State = grp.Key,
+                Total = grp.Sum(x => x.Total)
+            }).OrderByDescending(res => res.Total)
+            .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("ByCustomer/{number}")]
+        public IActionResult ByCustomer(int number)
+        {
+            var orders = _context.Orders.Include(_o => _o.Customer).ToList();
+
+            var groupedResult = orders.GroupBy(o => o.Customer.Id)
+                .ToList()
+                .Select(grp => new
+                {
+                    Name = _context.Customers.Find(grp.Key).Name,
+                    Total = grp.Sum(x => x.Total)
+                }).OrderByDescending(res => res.Total)
+                .Take(number)
+                .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("GetOrder", Name = "GetOrder")]
+        public IActionResult GetOrder(int id)
+        {
+            var order = _context.Orders.Include(_o => _o.Customer)
+                .First(o => o.Id == id);
+
+            return Ok(order);
         }
     }
 }
