7e9bb86bf62ab69a1c58a22af40a519a140b360f
DreiK-one <Dreikone1@mail.ru>
2022-12-21 12:09:00 +0100

    Add themes, pie chart functionality

--- frontend/src/app/charts/pie-chart/pie-chart.component.ts
+++ frontend/src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ChartData, ChartType } from 'chart.js';
+import _ from 'lodash';
+import { THEME_COLORS } from 'src/app/shared/theme.colors';
+
+
+const theme = 'Default';
 
 @Component({
   selector: 'app-pie-chart',
@@ -10,23 +15,39 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartLabels: string[] = ['XYZ Logic', 'Main Bakery', 'Acme Hosting'];
+  @Input() inputData: any;
+  @Input() limit: any;
+
+  chartData?: number[];
+  pieChartLabels?: string[];
   pieChartType: ChartType = 'doughnut';
-
-  pieChartData: ChartData<'doughnut'> = {
-    labels: this.pieChartLabels,
-    datasets: [
-      {
-        data: [350, 450, 120],
-        backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
-        borderColor: '#111'
-      }
-    ]
-  };
-
-  
+  pieChartData?: ChartData<'doughnut'>;
 
   ngOnInit(): void {
+    this.parseChartData(this.inputData, this.limit);
   }
 
+  parseChartData(res: any, limit?: number) {
+    const allData = res.slice(0, limit);
+    this.chartData = allData.map((x: any) => _.values(x)[1]);
+    this.pieChartLabels = allData.map((x: any) => _.values(x)[0]);
+
+    this.pieChartData = {
+      labels: this.pieChartLabels,
+      datasets: [
+        {
+          data: this.chartData as number[],
+          backgroundColor: this.themeColors(theme),
+          borderColor: '#111'
+        }
+      ]
+    };
+  }
+
+  themeColors(setName: string): string[] {
+    const colors = THEME_COLORS.slice(0)
+      .find(set => set.name === setName)?.colorSet;
+
+    return colors as string[];
+  }
 }
