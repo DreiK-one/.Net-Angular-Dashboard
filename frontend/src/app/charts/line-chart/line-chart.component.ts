8533f81cf76130d7c192bef2c9609bb8b1f9e887
DreiK-one <Dreikone1@mail.ru>
2022-12-22 12:45:41 +0100

    Finish line chart component

--- frontend/src/app/charts/line-chart/line-chart.component.ts
+++ frontend/src/app/charts/line-chart/line-chart.component.ts
@@ -1,14 +1,10 @@
+import { map } from 'rxjs';
+import { SalesDataService } from './../../services/sales-data.service';
 import { Component, OnInit } from '@angular/core';
 import { ChartData, ChartType, Colors } from 'chart.js';
+import moment from 'moment';
+import { U } from 'chart.js/dist/chunks/helpers.core';
 
-
-const LINE_CHART_SAMPLE_DATA: any[] = [
-  {data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analisis'},
-  {data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
-  {data: [52, 34, 49, 53, 68, 62], label: 'Forecasting'}
-];
-
-const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -17,74 +13,136 @@ const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 })
 export class LineChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesDataService: SalesDataService) { }
 
-  // lineChartData: ChartData<'line'> = {
-  //   labels: LINE_CHART_LABELS,
-  //   datasets: [
-  //     {
-  //       data: LINE_CHART_SAMPLE_DATA,
-  //       backgroundColor: ['rgba(6, 214, 160, 0.2)', 'rgba(255, 209, 102, 0.2)','rgba(15, 78, 133, 0.2)',],
-  //       borderColor: ['rgba(0, 200, 140, 0.5)', 'rgba(240, 180, 89, 0.5)', 'rgba(3, 64, 128, 0.5)'],
-  //       pointBackgroundColor: ['#000', '#000', '#000'],
-  //       pointBorderColor: ['#000', '#000', '#000'],
-  //       pointHoverBackgroundColor: ['#555', '#555', '#555'],
-  //       pointHoverBorderColor: ['#555', '#555', '#555']
-  //     }
-  //   ],
-  // };
-
-  lineChartData: ChartData<'line'> = {
-      labels: LINE_CHART_LABELS,
-      datasets: [
-        {
-          data: [32, 14, 46, 23, 38, 56],
-          label: '123',
-          backgroundColor: 'rgba(6, 214, 160, 0.2)',
-          borderColor: 'rgba(0, 200, 140, 0.5)',
-          pointBackgroundColor: '#000',
-          pointBorderColor: '#000',
-          pointHoverBackgroundColor: '#555',
-          pointHoverBorderColor: '#555',
-          tension: 0.3,
-          fill: true
-        },
-        {
-          data: [12, 18, 26, 13, 28, 26],
-          label: '456',
-          backgroundColor: 'rgba(255, 209, 102, 0.2)',
-          borderColor: 'rgba(240, 180, 89, 0.5)',
-          pointBackgroundColor: '#000',
-          pointBorderColor: '#000',
-          pointHoverBackgroundColor: '#555',
-          pointHoverBorderColor: '#555',
-          tension: 0.3,
-          fill: true
-        },
-        {
-          data: [52, 34, 49, 53, 68, 62],
-          label: '789',
-          backgroundColor: 'rgba(15, 78, 133, 0.2)',
-          borderColor: 'rgba(3, 64, 128, 0.5)',
-          pointBackgroundColor: '#000',
-          pointBorderColor: '#000',
-          pointHoverBackgroundColor: '#555',
-          pointHoverBorderColor: '#555',
-          tension: 0.3,
-          fill: true
-        },
-      ],
-    };
-
-  // lineChartData = LINE_CHART_SAMPLE_DATA;
-  lineChartLabels = LINE_CHART_LABELS;
-  lineChartOptions: any = {
-    responsive: true,
-  };
+  topCustomers?: string[];
+  allOrders?: any[];
+  lineChartData?: ChartData<'line'>;
+  lineChartLabels?: string[];
+  lineChartOptions: any = { responsive: true };
   lineChartLegend = true;
   lineChartType: ChartType = 'line';
 
   ngOnInit(): void {
-  }
+    this._salesDataService.getOrders(1, 100).subscribe(res => {
+      this.allOrders = (res as any)['page']['data'];
 
+      this._salesDataService.getOrdersByCustomer(3).subscribe(cus => {
+        this.topCustomers = (cus as any).map((x: any) => x['name']);
+
+        const allChartData = this.topCustomers?.reduce((result: any, i: any) => {
+          result.push(this.getChartData(this.allOrders, i));
+          return result;
+        }, []);
+
+        let dates = allChartData.map( (x: any) => x['data']).reduce((a: any, i: any) => {
+          a.push(i.map((o: any) => new Date(o[0])));
+          return a;
+        }, []);
+
+        dates = [].concat.apply([], dates);
+
+        const r = (this.getCustomerOrdersByDate(allChartData, dates) as any)['data'];
+
+        this.lineChartLabels = (r as any)[0]['orders'].map((o: any) => o['date']).reverse();
+
+        this.lineChartData = {
+          labels: this.lineChartLabels,
+          datasets: [
+            {
+              data: (r as any)[0].orders.map((x: any) => x.total),
+              label: (r as any)[0]['customer'],
+              backgroundColor: 'rgba(6, 214, 160, 0.2)',
+              borderColor: 'rgba(0, 200, 140, 0.5)',
+              pointBackgroundColor: '#000',
+              pointBorderColor: '#000',
+              pointHoverBackgroundColor: '#555',
+              pointHoverBorderColor: '#555',
+              tension: 0.3,
+              fill: true
+            },
+            {
+              data: (r as any)[1].orders.map((x: any) => x.total),
+              label: (r as any)[1]['customer'],
+              backgroundColor: 'rgba(255, 209, 102, 0.2)',
+              borderColor: 'rgba(240, 180, 89, 0.5)',
+              pointBackgroundColor: '#000',
+              pointBorderColor: '#000',
+              pointHoverBackgroundColor: '#555',
+              pointHoverBorderColor: '#555',
+              tension: 0.3,
+              fill: true
+            },
+            {
+              data: (r as any)[2].orders.map((x: any) => x.total),
+              label: (r as any)[2]['customer'],
+              backgroundColor: 'rgba(15, 78, 133, 0.2)',
+              borderColor: 'rgba(3, 64, 128, 0.5)',
+              pointBackgroundColor: '#000',
+              pointBorderColor: '#000',
+              pointHoverBackgroundColor: '#555',
+              pointHoverBorderColor: '#555',
+              tension: 0.3,
+              fill: true
+            },
+          ],
+        }
+      });
+    });
+  };
+
+  getChartData(allOrders: any, name: string) {
+    const customerOrders = allOrders.filter((o: any) => o.customer.name === name)
+
+    const formattedOrders = customerOrders.reduce((r: any, e: any) => {
+      r.push([e.placed, e.total]);
+      return r;
+    }, []);
+
+    const result = {customer: name, data: formattedOrders};
+
+    return result;
+  };
+
+  getCustomerOrdersByDate(orders: any, dates: any) {
+    const customers = this.topCustomers;
+    const prettyDates = dates.map((x: any) => this.toFriendlyDates(x));
+    const uniqueDates = Array.from(new Set(prettyDates)).sort();
+
+    const result = {};
+    const dataSets: any = (result as any)['date'] = [];
+
+    customers?.reduce((x: any, y: any, i: any) => {
+      const customerOrders: any[] = [];
+      dataSets[i] = {
+        customer: y, 
+        orders: uniqueDates.reduce((r: any, e: any, j: any) => {
+          const obj: any = {};
+          obj['data'] = e;
+          obj['total'] = this.getCustomerDateTotal(e, y);
+          customerOrders.push(obj);
+          return customerOrders;
+        }, [])
+      }
+      return x;
+    }, []);
+
+    return result;
+  };
+
+  toFriendlyDates(date: Date) {
+    return moment(date).endOf('day').format('DD.MM.YY');
+  };
+
+  getCustomerDateTotal(date: string, customer: string) {
+    const r = this.allOrders?.filter((o: any) => o.customer.name === customer 
+      && this.toFriendlyDates(o.placed) === date);
+
+    const result = (r as any[]).reduce((a: any, b: any) => {
+      return a + b.total;
+    }, 0);
+
+    return result;
+  };
 }
+
