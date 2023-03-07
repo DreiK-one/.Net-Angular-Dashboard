6f338e5879509121017ceb280f76531712f0562e
DreiK-one <Dreikone1@mail.ru>
2023-03-07 20:06:21 +0100

    Add validation for signup

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,15 +11,46 @@ export class SignupComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
   }
 
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? ((this.eyeIcon = "fa-eye") && (this.type = "text")) : 
         ((this.eyeIcon = "fa-eye-slash") && (this.type = "password"));
+  }
+
+  onSignUp(){
+    if(this.signUpForm.valid){
+      console.log(this.signUpForm.value);
+      //Perform logic for signup 
+    }else{
+      this.validateAllFormFields(this.signUpForm);
+      //logic for throwing error
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf: true});
+        
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control);
+      }
+    })
   }
 }
