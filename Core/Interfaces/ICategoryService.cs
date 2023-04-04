378734f0063c0f48e31e1eea65b1a125d19dabd9
DreiK-one <Dreikone1@mail.ru>
2023-04-04 20:29:37 +0200

    Add interfaces

---
+++ Core/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+
+
+namespace Core.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(int id);
+        Task<int> CreateCategory(Category category);
+        Task<int> DeleteCategory(int id);
+        Task<int> UpdateCategory(Category category);
+    }
+}
